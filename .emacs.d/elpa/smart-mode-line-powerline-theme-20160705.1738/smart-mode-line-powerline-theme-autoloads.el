9f6acb67203d931f6b2ffb2e4d408c1963ed9779
bwall72 <brandonw@live.com>
2018-05-14 03:55:29 -0400

    moving to org file

--- .emacs.d/elpa/smart-mode-line-powerline-theme-20160705.1738/smart-mode-line-powerline-theme-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; smart-mode-line-powerline-theme-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "smart-mode-line-light-powerline-theme" "smart-mode-line-light-powerline-theme.el"
-;;;;;;  (23088 59509 896534 749000))
-;;; Generated autoloads from smart-mode-line-light-powerline-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil "smart-mode-line-powerline-theme" "smart-mode-line-powerline-theme.el"
-;;;;;;  (23088 59509 884534 893000))
-;;; Generated autoloads from smart-mode-line-powerline-theme.el
-
-(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
-
-;;;***
-
-;;;### (autoloads nil nil ("smart-mode-line-powerline-theme-pkg.el")
-;;;;;;  (23088 59509 908534 606000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; smart-mode-line-powerline-theme-autoloads.el ends here
